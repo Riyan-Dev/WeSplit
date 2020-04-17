090bbd8c8027250f8bffabe6ea1d967a96f815c4
Muhammad Riyan <riyan20riyan@icloud.com>
2020-04-17 18:20:45 +0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Muhammad Riyan on 17/04/2020.
+//  Copyright © 2020 Muhammad Riyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
